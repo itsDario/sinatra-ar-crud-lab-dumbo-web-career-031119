4c43c961c8f649d9984a9a596696ebeb018a9f7a
github username <itsDario1@gmail.com>
2019-04-04 10:11:09 -0400

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,8 +6,60 @@ class ApplicationController < Sinatra::Base
   configure do
     set :public_folder, 'public'
     set :views, 'app/views'
+    set :method_override, 'true'
+
   end
 
-  get '/' do
+  get '/articles/new' do          #NEW ARTICLE FORM
+    erb :new
   end
+
+  post '/articles' do            #NEW ARTICLE CREATION AND REDIRECT
+    article_title = params[:title]
+    article_content = params[:content]
+    @article = Article.create(title: article_title, content: article_content)
+    # binding.pry
+    redirect '/articles/#{@new_artsist}' + @article.id.to_s
+  end
+
+  get '/' do                     #REDIRECT TO INDEX
+    redirect '/articles'
+  end
+
+  get '/articles/:id' do          #READ AND SHOW AN ARTICLE
+    # binding.pry
+    @this_article = Article.find(params[:id])
+    erb :show
+  end
+
+  get '/articles' do              #READ AND SHOW ALL ARTICLES
+    @all_articles = Article.all # this 
+    # binding.pry
+
+    erb :index
+  end
+
+  get '/articles/:id/edit' do     #EDIT FORM FOR AND ARTICLE
+    @article = Article.find(params[:id])
+    # binding.pryk
+    erb :edit
+  end
+
+  patch '/articles/:id' do       #APPLY AND REDIRECT EDITS TO AN ARTICLE
+    @article = Article.find(params[:id])
+    # binding.pry
+    @article.update(params[:article])#   next param then update
+    # binding.pry
+    # redirect '/articles/#{params[:id].to_i}'# redirect
+    redirect to '/articles/' + params[:id].to_s# redirect    
+  end
+
+  delete '/articles/:id' do       #APPLY AND REDIRECT EDITS TO AN ARTICLE
+    @article = Article.find(params[:id])
+    # binding.pry
+    Article.delete(params[:id])#   next param then update
+    # redirect '/articles/#{params[:id].to_i}'# redirect
+    redirect to '/'# redirect to index
+  end
+
 end
